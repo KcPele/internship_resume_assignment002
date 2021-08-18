eeae115e46aa10f5fc09b7976079f768e789643c
KcPele <64108987+KcPele@users.noreply.github.com>
2021-08-18 03:02:35 +0100

    another45

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -139,7 +139,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'fidekg123@gmail.com'
-EMAIL_HOST_PASSWORD = 'kcpele123@'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
