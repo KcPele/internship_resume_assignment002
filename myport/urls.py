dcb107e629f2cbd4341c65cff9bec7be92085b00
KcPele <64108987+KcPele@users.noreply.github.com>
2021-08-18 01:26:05 +0100

    Update urls

--- myport/urls.py
+++ myport/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
@@ -9,3 +10,5 @@ urlpatterns = [
    path('email-contact/', views.email_contact, name='email_contact'),
    path('email-subscribe', views.email_subscribe, name='email_subscribe'),
 ]
+
+urlpatterns += staticfiles_urlpatterns
