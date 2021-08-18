a66f2be881b39dad390e906f0ce658b6c1c8cc5e
KcPele <64108987+KcPele@users.noreply.github.com>
2021-08-18 02:08:25 +0100

    another002

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,10 +124,11 @@ USE_TZ = True
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
